d6b1ee45c814632e8aa91d11eb15b0d8d2fc68c2
Sajib Das <sajibdas9254@gmail.com>
2026-09-09 14:22:06 +0300

    refactor: convert Tours to use local state

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,30 +1,36 @@
-function Tour({ image, date, title, info, location, duration, cost, difficulty, rating }) {
+const Tour = ({ id, image, date, title, info, location, duration, cost, removeTour }) => {
   return (
-    <article className="tour-card">
-      <div className="tour-img-container">
-        <img src={image} className="tour-img" alt={title} />
-        <p className="tour-date">{date}</p>
-        <p className="tour-difficulty">{difficulty}</p>
+    <article className='tour-card'>
+      <div className='tour-img-container'>
+        <img src={image} className='tour-img' alt={title} />
+        <p className='tour-date'>{date}</p>
       </div>
-      <div className="tour-info">
-        <div className="tour-title">
+      <div className='tour-info'>
+        <div className='tour-title'>
           <h4>{title}</h4>
-          <p className="tour-rating">⭐ {rating}</p>
         </div>
         <p>{info}</p>
-        <div className="tour-footer">
+        <div className='tour-footer'>
           <p>
             <span>
-              <i className="fas fa-map" />
-            </span>
+              <i className='fas fa-map'></i>
+            </span>{' '}
             {location}
           </p>
-          <p>from {cost}</p>
           <p>{duration} days</p>
+          <p>from ${cost}</p>
         </div>
+        <button
+          type='button'
+          className='btn'
+          onClick={() => removeTour(id)}
+          style={{ marginTop: '1rem', cursor: 'pointer' }}
+        >
+          not interested
+        </button>
       </div>
     </article>
-  );
+  )
 }
 
-export default Tour;
+export default Tour
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,18 +1,32 @@
-import { tours } from "../data";
-import Title from "./Title";
-import Tour from "./Tour";
+import { useState } from 'react'
+import { tours } from '../data'
+import Title from './Title'
+import Tour from './Tour'
 
 const Tours = () => {
-  return (
-    <section className="section" id="tours">
-      <Title title="featured" subTitle="tours" />
+  const [toursData, setToursData] = useState(tours)
 
-      <div className="section-center featured-center">
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+  const handleRemove = (id) => {
+    setToursData(toursData.filter((tour) => tour.id !== id))
+  }
+
+  return (
+    <section className='section' id='tours'>
+      <Title title='featured' subTitle='tours' />
+
+      <div className='section-center featured-center'>
+        {toursData.map((tour) => {
+          return (
+            <Tour
+              {...tour}
+              key={tour.id}
+              removeTour={handleRemove}
+            />
+          )
         })}
       </div>
     </section>
-  );
-};
-export default Tours;
+  )
+}
+
+export default Tours
